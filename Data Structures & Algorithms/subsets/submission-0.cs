18ce2e3a99d31276b97c52e77603c8ea315d6c95
kk08bce026 <69952570+kk08bce026@users.noreply.github.com>
2026-06-06 18:07:20 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public List<List<int>> Subsets(int[] nums) 
+    {
+        List<List<int>> ans = new List<List<int>>();
+        List<int> curr = new List<int>();
+        helperSubSet(curr, 0, nums, ans);
+        return ans;
+        
+    }
+    public void helperSubSet(List<int> curr, int i, int[] nums, List<List<int>> ans)
+    {
+        if(i==nums.Length)
+        {
+            // add clone
+            ans.Add(new List<int>(curr));
+            return;
+        }
+
+        // include
+        curr.Add(nums[i]);
+        helperSubSet(curr, i+1, nums, ans);
+
+        //backtracking step to go to prev place list is mutable so anyone can change
+        curr.RemoveAt(curr.Count-1);
+        
+        // Exclude
+        helperSubSet(curr, i+1, nums, ans);
+    }
+}
